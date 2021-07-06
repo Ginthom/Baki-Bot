44b8943862774d79966004a37cdbe5d239b632c2
Thomas <77125551+Ginthom@users.noreply.github.com>
2021-07-06 19:28:12 +0200

    Add files via upload
    fixed an exploit with which all users could access all role-pings including @everyone and @here

--- commands/gotobrazil.js
+++ commands/gotobrazil.js
@@ -7,6 +7,27 @@ module.exports = {
 }
 
 function gotobrazil(message, args) {
-    message.channel.send(`${args[0]} was shipped to brazil by Baki-Bot`);
+    let allowed = true;
+    let roles = message.guild.roles.cache;
+
+    roles.forEach(e => { 
+        if(e.id == args[0].replace('@', '').replace('<', '').replace('>', '').replace('&', '')) {
+            allowed = false;
+
+        } 
+
+    });
+
+
+    if (args[0] != "@everyone" &&
+        args[0] != "@here" &&
+        allowed) {
+        message.channel.send(`${args[0]} was shipped to brazil by Baki-Bot`);
+
+    } else {
+        message.channel.send("Illegal cargo, I don't wanna go to jail :C");
+
+    }
+    
 
 }
